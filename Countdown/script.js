f5504fb210ed9a35b3de66ee3479761374529b67
Lais Luderer <lais.alvarenga.global@gmail.com>
2022-11-01 17:35:38 -0300

    HTML e CSS do Countdown

--- Countdown/script.js
+++ Countdown/script.js
@@ -1,26 +1,28 @@
-const clock = document.getElementById('time');
-const timeFinish = new Date("Nov 01, 2022 00:00:00").getTime();
+const clock = document.querySelector('.time');
+const timeFinish = new Date("Nov 10, 2022 00:00:00").getTime();
 
 function countdown() {
-    const timeNow = new Date().getTime();
+  const timeNow = new Date().getTime();
 
-    const diferencaTempo = timeFinish - timeNow;
+  const diferencaTempo = timeFinish - timeNow;
 
-    //Conversão dos valores
-    const days = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (diferencaTempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor(
-      (diferencaTempo % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    const seconds = Math.floor((diferencaTempo % (1000 * 60)) / 1000);
-    
-    hours < 10 ? clock.innerHTML = days + "d " + "0" + hours + "h " + minutes + "m " + seconds + "s " : clock.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  //Conversão dos valores
+  const days = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (diferencaTempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor(
+    (diferencaTempo % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  const seconds = Math.floor((diferencaTempo % (1000 * 60)) / 1000);
 
-    minutes < 10 ? clock.innerHTML = days + "d " + hours + "h " + "0" + minutes + "m " + seconds + "s " : clock.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  days < 10 ? document.getElementById('day').innerHTML = "0" + days + " :" : document.getElementById('day').innerHTML = days;
 
-    days < 10 ? clock.innerHTML = "0" + days + "d " + hours + "h " + minutes + "m " + seconds + "s " : clock.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  hours < 10 ? document.getElementById('hour').innerHTML = "0" + hours + " :" : document.getElementById('hour').innerHTML = hours;
 
-    setTimeout(countdown, 1000);
+  minutes < 10 ? document.getElementById('minute').innerHTML = "0" + minutes + " :": document.getElementById('minute').innerHTML = minutes + " :";
+
+  seconds < 10 ? document.getElementById('second').innerHTML = "0" + seconds : document.getElementById('second').innerHTML = seconds;
+
+  setTimeout(countdown, 1000);
 }
